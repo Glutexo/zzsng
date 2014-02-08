fab7a4f1e5cfdb0ece9d216e7d98234d21e06811
Glutexo <glutexo@me.com>
2014-02-08 20:22:40 +0100

    Functional inserting into a split table.
    When inserting a new row, the last partition of a split table
    is determined. If there is enough space in the table for
    another row to be inserted, it is put there. Otherwise a new
    partition is created. Non-split tables work as usual.
    
    Conflicts:
    	include/db_config.php

--- lang/en.php
+++ lang/en.php
@@ -3,6 +3,8 @@ class master_lang {
     const unsupported_database_type = "Unsupported database type";
     const database_connection_error = "Database connection error";
     const database_selection_error = "Database selection error";
+	const no_row_limit_set = "Row limit is not set, split tables can’t be used.";
+	const comparison_error = "Comparison/sort error.";
 
     const value_pairs_must_be_array = "Value pairs must be an array.";
     const sql_pairs_must_be_array = "SQL pairs must be an array.";
