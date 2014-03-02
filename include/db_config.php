cc0b3f2fbd0dcf329cc37906547b26351462083a
Glutexo <driezasson@me.com>
2014-03-02 20:54:23 +0100

    Sample DB config file.

---
+++ include/db_config.php
@@ -0,0 +1,11 @@
+<?php
+class DbConfig {
+    const TYPE = "pgsql";
+    const SERVER = "localhost";
+    const PORT = 5432;
+    const USERNAME = "root";
+    const PASSWORD = "root";
+    const DB = "zzsng";
+    const ENCODING = "utf8";
+}
+?>
