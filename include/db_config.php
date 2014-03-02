8f8e5fa67d51a820d617640d80edb184438c1737
Glutexo <driezasson@me.com>
2014-03-02 20:56:48 +0100

    DB config file.

---
+++ include/db_config.php
@@ -0,0 +1,11 @@
+<?php
+class DbConfig {
+    const TYPE = "pgsql";
+    const SERVER = "localhost";
+    const PORT = 5432;
+    const USERNAME = "php";
+    const PASSWORD = "Polskie Koleje Państwowe";
+    const DB = "zzsng";
+    const ENCODING = "utf8";
+}
+?>
